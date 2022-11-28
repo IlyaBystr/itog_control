ceccdc6d1df2c4b3d7cc2aaeb833070799bbdfed
Ilya <fox367213@mail.com>
2022-11-28 11:51:17 +0300

    creating new metod

--- Program.cs
+++ Program.cs
@@ -1,14 +1,47 @@
-﻿Console.WriteLine("Введите колличество элементов в массиве");
-
+﻿
+Console.WriteLine("Введите колличество элементов в массиве");
 int a = int.Parse(Console.ReadLine()!);
-Printarray(a);
+string[] arr = new string[a];
+CreatArray(a, arr);
+
+Console.WriteLine("Введите колличество символов в новом массиве");
+int b = int.Parse(Console.ReadLine()!);
+string[] Arry = new string[10];
+CreatNewArray(b, Arry);
+
+void CreatArray(int a, string[] arr)
+{
+    for (int i = 0; i < a; ++i)
+    {
+        arr[i] = Console.ReadLine()!;
+        Console.WriteLine($"Вы ввели {i + 1} элемент массиыв");
+    }
+    Console.Write("[ ");
+    for (int b = 0; b < arr.Length; b++)
+    {
+        Console.Write(arr[b] + " ");
+    }
+    Console.WriteLine("]");
 
 
+}
 
+void CreatNewArray(int b, string[] Array)
+{
+    int l = 0;
 
-
-void Printarray(int a){
-int[] arr = new int[a];
-for (int i = 0; i < a; ++i)
-arr[i] = int.Parse(Console.ReadLine()!);
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= b)
+        {
+            Array[l] = arr[i];
+            l++;
+        }
+    }
+    Console.Write("[ ");
+    for (int j = 0; j < Array.Length; j++)
+    {
+        Console.Write(Array[j] + " ");
+    }
+    Console.WriteLine("]");
 }
