7fdd0bd04a0c95cdc3b2b63fd311464f718a0f81
Ilya <fox367213@mail.com>
2022-11-28 09:43:44 +0300

    creat array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите колличество элементов в массиве");
+
+int a = int.Parse(Console.ReadLine()!);
+Printarray(a);
+
+
+
+
+
+void Printarray(int a){
+int[] arr = new int[a];
+for (int i = 0; i < a; ++i)
+arr[i] = int.Parse(Console.ReadLine()!);
+}
